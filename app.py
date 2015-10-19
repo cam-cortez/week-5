bad9a59f982f4c3c6ed187ffbe1d3375506b081c
cam-cortez <cdc2184@columbia.edu>
2015-10-18 22:40:58 -0400

    edits!

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask import render_template
 from flask import request
 from flask import Response
 
-import json
-import time
-import sys
 import random
+
+import math
+
+import json
+
+import sys
 
 import pyorient
 
@@ -20,12 +23,11 @@ def event_stream():
     while True:
         result = q.get()
         yield 'data: %s\n\n' % str(result)
-
+        
 @app.route('/eventSource/')
 def sse_source():
-    return Response(
-            event_stream(),
-            mimetype='text/event-stream')
+        return Response( event_stream(), mimetype='text/event-stream' )
+        
 
 @app.route("/")
 def index():
@@ -34,17 +36,20 @@ def index():
 @app.route("/getData/")
 def getData():
 
-	q.put("starting data query...")
-
-	lat1 = str(request.args.get('lat1'))
-	lng1 = str(request.args.get('lng1'))
-	lat2 = str(request.args.get('lat2'))
-	lng2 = str(request.args.get('lng2'))
-
-	print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
+        q.put("starting data query...")
+    
+        lat1 = str(request.args.get('lat1'))
+        lng1 = str(request.args.get('lng1'))
+        lat2 = str(request.args.get('lat2'))
+        lng2 = str(request.args.get('lng2'))
+        w = float(request.args.get('w'))
+        h = float(request.args.get('h'))
+        cell_size = float(request.args.get('cell_size'))
+        
+        print "received coordinates: [" + lat1 + ", " + lat2 + "], [" + lng1 + ", " + lng2 + "]"
 	
-	client = pyorient.OrientDB("localhost", 2424)
-	session_id = client.connect("root", "password")
+        client = pyorient.OrientDB("localhost", 2424)
+	session_id = client.connect("root", "spot")
 	db_name = "soufun"
 	db_username = "admin"
 	db_password = "admin"
@@ -55,13 +60,13 @@ def getData():
 	else:
 		print "database [" + db_name + "] does not exist! session ending..."
 		sys.exit()
+		
 
 	query = 'SELECT FROM Listing WHERE latitude BETWEEN {} AND {} AND longitude BETWEEN {} AND {}'
 
 	records = client.command(query.format(lat1, lat2, lng1, lng2))
-
 	random.shuffle(records)
-	records = records[:100]
+        records = records[:100]
 
 	numListings = len(records)
 	print 'received ' + str(numListings) + ' records'
@@ -78,10 +83,31 @@ def getData():
 		feature["geometry"]["coordinates"] = [record.latitude, record.longitude]
 
 		output["features"].append(feature)
+		
+        q.put('starting analysis...')
+        
+        output["analysis"] = []
+        
+        numW = int(math.floor(w/cell_size))
+        numH = int(math.floor(h/cell_size))
+        
+        for j in range(numH):
+                for i in range(numW):
+                        newItem = {}
 
-	q.put('idle')
+                        newItem['x'] = i*cell_size
+                        newItem['y'] = j*cell_size
+                        newItem['width'] = cell_size-1
+                        newItem['height'] = cell_size-1
+                        newItem['value'] = .5
 
+                        output["analysis"].append(newItem)
+                        
+                        q.put('idle')
+        
 	return json.dumps(output)
+	
+output = {"type":"FeatureCollection","features":[]}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
